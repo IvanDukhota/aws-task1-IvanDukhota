c76b55f5a37f67e49b5d5aa54fcad39564b91192
Ivan Dukhota <ivan.dukhota@gmail.com>
2026-09-21 12:12:22 +0300

    Add VPC infrastructure and terraform output

--- main.tf
+++ main.tf
@@ -1,1 +1,110 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main-vpc"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.publicCIDR
+  availability_zone = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+    Environment = var.environment
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-rt"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "instance" {
+  name = "instance-sg"
+  vpc_id = aws_vpc.main.id
+
+  dynamic "ingress" {
+    for_each = var.allowed_ports
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "instance-sg"
+    Environment = var.environment
+  }
+}
+
+resource "aws_instance" "web" {
+  ami = var.instance_AMI
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              EOF
+
+  tags = {
+    Name = var.instance_tag
+    Environment = var.environment
+  }
+}
